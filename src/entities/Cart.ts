7231e5dfff7750c9d1ce4a06aa607cfac4149b8a
phpontes <ppontes95@hotmail.com>
2025-03-27 10:58:43 -0300

    aplicando conceito stateless na API

--- src/entities/Cart.ts
+++ src/entities/Cart.ts
@@ -14,13 +14,17 @@ export class Cart {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @OneToMany(() => CartItem, (cartItem) => cartItem.product)
+  @Column()
+  @Generated("uuid")
+  uuid: string;
+
+  @OneToMany(() => CartItem, (cartItem) => cartItem.product, { eager: true})
   items: CartItem[];
 
   @ManyToOne(() => Customer)
   customer: Customer | null;
 
-  @Column()
+  @CreateDateColumn()
   createdAt: Date;
 }
 
